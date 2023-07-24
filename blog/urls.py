c4038cf3aa525b683224c874ccf5ba3630b525ae
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-07-24 17:06:44 +0300

    Маршруты для категорий

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("update-user/<int:pk>/", UpdateUser.as_view(), name="update_user"),
     path("logout-user/", user_logout, name="logout_user"),
     path("delete-user/<int:pk>/", DeleteUser.as_view(), name="delete_user"),
+    path("create-category/", CreateCategory.as_view(), name="create_category"),
+    path("list-category/", ListCategory.as_view(), name="list_category"),
+    path("update-category/<int:pk>/", UpdateCategory.as_view(), name="update_category"),
+    path("delete-category/<int:pk>/", DeleteCategory.as_view(), name="delete_category"),
 ]
